56340150636a4d4ab084776ea366bcf396c9c06c
Marco Schlumpp <marco.schlumpp@gmail.com>
2021-01-15 21:41:06 +0100

    Improve keybindings of lsp

--- lua/lspinit.lua
+++ lua/lspinit.lua
@@ -62,39 +62,43 @@ local servers = {
 }
 
 local function make_on_attach(config) 
-    return function(client)
+    return function(client, bufnr)
+        local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+        local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+
         if config.before then config.before(client) end
 
         completion.on_attach()
         lsp_status.on_attach(client)
         local opts = {noremap = true, silent = true}
-        vim.api.nvim_buf_set_keymap(0, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
-        vim.api.nvim_buf_set_keymap(0, 'n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-        vim.api.nvim_buf_set_keymap(0, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-        vim.api.nvim_buf_set_keymap(0, 'n', 'gy', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-        vim.api.nvim_buf_set_keymap(0, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-        vim.api.nvim_buf_set_keymap(0, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-        vim.api.nvim_buf_set_keymap(0, 'n', '<c-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-        vim.api.nvim_buf_set_keymap(0, 'n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-        vim.api.nvim_buf_set_keymap(0, 'n', '<leader>m', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-        vim.api.nvim_buf_set_keymap(0, 'n', ']g', '<cmd>lua vim.lsp.diagnostic.goto_next()<cr>', opts)
-        vim.api.nvim_buf_set_keymap(0, 'n', '[g', '<cmd>lua vim.lsp.diagnostic.goto_prev()<cr>', opts)
+        buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+        buf_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+        buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+        buf_set_keymap('n', 'gy', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+        buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+        buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+        buf_set_keymap('', '<c-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+        buf_set_keymap('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+        buf_set_keymap('n', '<M-CR>', '<cmd>lua require"telescope.builtin".lsp_code_actions(require("telescope.themes").get_dropdown({ winblend = 5 }))<CR>', opts)
+        buf_set_keymap('v', '<leader>m', '<cmd>lua vim.lsp.buf.range_code_action()<CR>', opts)
+        buf_set_keymap('n', ']g', '<cmd>lua vim.lsp.diagnostic.goto_next()<cr>', opts)
+        buf_set_keymap('n', '[g', '<cmd>lua vim.lsp.diagnostic.goto_prev()<cr>', opts)
 
-        -- TODO: Check if convenient
-        vim.api.nvim_buf_set_keymap(0, 'n', '<leader>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
-        vim.api.nvim_buf_set_keymap(0, 'n', '<leader>E', '<cmd>lua vim.lsp.diagnostic.set_loclist()<cr>', opts)
+        buf_set_keymap('n', '<leader>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
+        buf_set_keymap('n', '<leader>E', '<cmd>lua vim.lsp.diagnostic.set_loclist()<cr>', opts)
 
         if client.resolved_capabilities.document_formatting then
-            vim.api.nvim_buf_set_keymap(0, 'n', '<leader>f', '<cmd>lua vim.lsp.buf.formatting()<cr>',
-            opts)
+            buf_set_keymap('n', '<leader>=', '<cmd>lua vim.lsp.buf.formatting()<cr>', opts)
         end
 
         if client.resolved_capabilities.document_highlight then
-            vim.api.nvim_command('augroup lsp_aucmds')
-            vim.api.nvim_command('au CursorHold <buffer> lua vim.lsp.buf.document_highlight()')
-            vim.api.nvim_command('au CursorHoldI <buffer> lua vim.lsp.buf.document_highlight()')
-            vim.api.nvim_command('au CursorMoved <buffer> lua vim.lsp.buf.clear_references()')
-            vim.api.nvim_command('augroup END')
+            lspconfig.util.nvim_multiline_command [[
+                augroup lsp_aucmds
+                au CursorHold <buffer> lua vim.lsp.buf.document_highlight()
+                au CursorHoldI <buffer> lua vim.lsp.buf.document_highlight()
+                au CursorMoved <buffer> lua vim.lsp.buf.clear_references()
+                augroup END
+            ]]
         end
 
         if config.after then config.after(client) end
