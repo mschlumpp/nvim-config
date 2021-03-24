c0a64c48fbd18b31bf287b9e3203daf156906b12
Marco Schlumpp <marco.schlumpp@gmail.com>
2021-03-24 12:09:47 +0100

    Enable emoji completions

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -7,6 +7,7 @@ require('compe').setup {
         calc = true,
         vsnip = true,
         nvim_lsp = true,
+        emoji = true,
         treesitter = {
             ignored_filetypes = {'rust', 'cpp'},
         },
