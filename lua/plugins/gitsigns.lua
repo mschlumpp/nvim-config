eae63e02d29cb7ef8f8bed3ae6f2aa44ca358a87
Marco Schlumpp <marco.schlumpp@gmail.com>
2021-08-02 13:08:36 +0200

    Add keybindings to stage/unstage using gitsigns

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -8,6 +8,8 @@ require 'gitsigns'.setup {
 
         ['n <leader>ghs'] = '<cmd>lua require"gitsigns".stage_hunk()<CR>',
         ['n <leader>ghu'] = '<cmd>lua require"gitsigns".undo_stage_hunk()<CR>',
+        ['x <leader>ghs'] = '<cmd>lua require"gitsigns".stage_hunk()<CR>',
+        ['x <leader>ghu'] = '<cmd>lua require"gitsigns".undo_stage_hunk()<CR>',
         ['n <leader>ghr'] = '<cmd>lua require"gitsigns".reset_hunk()<CR>',
         ['n <leader>ghp'] = '<cmd>lua require"gitsigns".preview_hunk()<CR>',
         ['n <leader>ghb'] = '<cmd>lua require"gitsigns".blame_line()<CR>',
