12aecabbd9a763aee937e12b05667af7f9dffc5d
Marco Schlumpp <marco.schlumpp@gmail.com>
2023-05-31 22:30:54 +0200

    Adjust to newer github colorscheme version

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -18,7 +18,7 @@ return {
         lazy = false,
         priority = 1000,
         config = function()
-            vim.cmd('colorscheme github_light_default')
+            vim.cmd.colorscheme('github_dark_dimmed')
         end,
     },
     {
