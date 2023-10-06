6896a046021091352efff776ebe442cc424637cb
Marco Schlumpp <marco.schlumpp@gmail.com>
2023-10-06 22:18:03 +0200

    Enable cmp source for crates.nvim
    The source is disabled in newer version of the plugin.

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -133,6 +133,11 @@ return {
             'nvim-lua/plenary.nvim',
         },
         opts = {
+            src = {
+                cmp = {
+                    enabled = true
+                },
+            },
         },
         config = function(plugin, opts)
             require'crates'.setup(opts)
