e7b794bfb9d220cef61caecc323496f6e9ec2a4e
Julia Schlumpp <julia@schlumpp.com>
2024-12-25 14:12:26 +0100

    Fix inlay hint toggle for new neovim versions

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -180,7 +180,7 @@ return {{
                 if client.server_capabilities.inlayHintProvider then
                     vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
                     vim.keymap.set('n', '<leader>th', function ()
-                        vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled(bufnr), { lbufnr = bufnr })
+                        vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = bufnr }), { bufnr = bufnr })
                     end, { buffer = bufnr, silent = true, desc = 'inlay-hints'})
                 end
 
